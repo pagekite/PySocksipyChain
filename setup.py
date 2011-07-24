d3bd880e7c75b807a613dbcaaea0d62f4533d1fd
Bjarni R. Einarsson <bre@klaki.net>
2011-07-24 10:23:06 +0000

    Cleanup

--- setup.py
+++ setup.py
@@ -4,14 +4,18 @@ from distutils.core import setup
 VERSION = "2.00"
 
 setup(
-name = "SocksiPy-chain",
-version = VERSION,
-description = "A Python SOCKS/HTTP Proxy module",
-long_description = "This Python module allows you to create TCP connections through a chain of SOCKS or HTTP proxies without any special effort.",
-url = "http://github.com/PageKite/SocksiPyChain",
-author = "Bjarni R. Einarsson",
-author_email="bre@pagekite.net",
-license = "BSD",
-py_modules=["socks"]
+    name = "SocksipyChain",
+    version = VERSION,
+    description = "A Python SOCKS/HTTP Proxy module",
+    long_description = """\
+This Python module allows you to create TCP connections through a chain
+of SOCKS or HTTP proxies without any special effort. It also supports
+TLS/SSL encryption if the OpenSSL modules are installed..",
+""",
+     url = "http://github.com/PageKite/SocksiPyChain",
+     author = "Bjarni R. Einarsson",
+     author_email="bre@pagekite.net",
+     license = "BSD",
+     py_modules=["sockschain"]
 )
 
