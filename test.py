eaa8af8a915fa80df5ee0fc80f57fd9444bd20d9
Bjarni R. Einarsson <bre@klaki.net>
2025-03-26 11:54:56 +0000

    Drop python3 and six, update IP check url. Fixes #16, Fixes #15

--- test.py
+++ test.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python
-from __future__ import print_function
-from six.moves.urllib.request import urlopen
 
 import telnetlib
-
-try:
-    import urllib.request as urllib_request  # Python 3
-except ImportError:
-    import urllib2 as urllib_request  # Python 2
+import urllib.request as urllib_request  # Python 3
+from urllib.request import urlopen
 
 # Import SocksiPy
 import sockschain as socks
@@ -19,7 +14,7 @@ socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 9050)
 
 # Route an HTTP request through the SOCKS proxy 
 socks.wrapmodule(urllib_request)
-print('\n%s\n' % urlopen('https://ipconfig.me/').read())
+print('\n%s\n' % urlopen('https://wtfismyip.com/text').read())
 
 # Route a telnet connection through the SOCKS proxy
 socks.wrapmodule(telnetlib)
